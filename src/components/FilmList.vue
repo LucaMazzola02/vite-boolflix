be77362a277cb02754f8c255cecd12e1a7111fd4
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-06-23 13:18:44 +0200

    add correct function for the flags languages

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,20 +1,28 @@
 <template>
-      <article v-for="movie in movies" class="card mb-5 col-6 p-0">
+  <div class="container">
+    <div class="row">
+      <article v-for="movie in movies" class="card mb-5 col-12 p-0">
         <div class="card-body text-center">
             <h2 class="card-title fw-bold">
                 {{ movie.title }}   
             </h2>
             <em><h4>{{ movie.original_title }}</h4></em>
+             
+            <img v-if="isLanguageAvailable(movie.original_language)" 
+            :src="getImagePath(movie.original_language)" alt="language-image"/> 
+
+            <span v-else>
+              {{ movie.original_language }}
+            </span>
+            
             <p class="movies-vote">
                 VOTO : {{ movie.vote_average }} 
-            </p>   
-
-            <p v-if="!availableLanguages.includes(movie.orginal_language)" class="movies-language">
-              {{ movie.orginal_language }}
-            </p>
-            <img v-else :src="`/flags/$(movie.orginal_language).png`"/>
+            </p> 
         </div>
-    </article>
+      </article>
+    </div>
+  </div>
+      
 </template>
 
 <script>
@@ -23,12 +31,26 @@ export default {
     props:{
       movies : Array
     },
-    data : {
-      return : {
-        availableLanguages : ["it","en","es","fr","ja","dt"]
+    data (){
+      return {
+        availableLanguages : ['it.png','en.png','es.png','fr.png','ja.png','dt.png']
       }
     },
-}
+
+    methods :{
+      isLanguageAvailable(language){
+        if(this.availableLanguages.includes(language + '.png')){
+          return true
+        }
+
+        return false;
+      },
+      getImagePath(img){
+        return new URL(`../assets/flags/${img}.png`, import.meta.url).href;
+      }
+
+    },
+}    
 </script>
 
 
@@ -38,3 +60,5 @@ export default {
 article img{width: 30px;}
 
 </style>
+
+
